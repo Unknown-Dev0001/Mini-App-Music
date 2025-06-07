52145e7be29973cfc6a1b5907d0af1f4998acebd
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2025-06-07 13:11:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,17 @@
 import os
 
 # Telegram Bot Credentials
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", 17609888))  # Replace with your API ID
+API_HASH = os.getenv("API_HASH", "6538740296c179c5f5a2c52f911864d1")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6720362606:AAECqT4oLm7CNeV57kDhgw3thTFfFoDMB8k")
 
-# MongoDB URI
-MONGO_URL = os.getenv("MONGO_URL", "")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://mongodbflex:L3GxCeWU3UcgFZZX-hwjiwjve@cluster0.czbo8.mongodb.net/?retryWrites=true&w=majority")
 
-# Logging Group (for logs)
+# Log group to receive logs when users start the bot or play songs
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", -1002078575375))
+
+# Default YouTube thumbnail if not found
+YT_THUMBNAIL = os.getenv("YT_THUMBNAIL", "https://i.ibb.co/JFr2D1nZ/photo-2025-06-04-08-20-01-7512006038174826500.jpg")
 
 # Mini App Base URL (e.g., Render or Vercel deployed web app)
 MINI_APP_URL = os.getenv("MINI_APP_URL", "")
-
-# Fallback thumbnail
-YT_THUMBNAIL = os.getenv("YT_THUMBNAIL", "https://i.ibb.co/Q3J5VnZq/Img2url-bot.jpg")
